45c0b5d8e8eff2b0b3f794ecd661a14809d09912
Ankit Hans <ankithans1947@gmail.com>
2021-06-05 17:49:08 +0530

    refactor flags & change exec func

--- pkg/pluginloader/loader.go
+++ pkg/pluginloader/loader.go
@@ -1,8 +1,6 @@
 package pluginloader
 
 import (
-	"bytes"
-	"io"
 	"os"
 	"os/exec"
 	"strconv"
@@ -14,47 +12,56 @@ import (
 // Unmarshal the YAML
 // Extract the commands using plugin.go (command structure)
 
-// Create a Cobra Command
-func createCommand(cmdStruct *CommandConfig) *cobra.Command {
+// Create a Cobra Command New
+func createCommandNew(cmdStruct *CommandConfig) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     cmdStruct.Name,
 		Short:   cmdStruct.ShortDescription,
 		Example: cmdStruct.Examples,
 		Args:    cobra.ExactArgs(len(cmdStruct.MapsTo.Args)),
 		RunE: func(cmd *cobra.Command, args []string) error {
+
+			// TODO: arguments are not typed
+			// append subcommand before the []args
 			args = append([]string{cmdStruct.MapsTo.Subcommand}, args...)
-			c := exec.Command(cmdStruct.MapsTo.Name, args...)
-			c.Stdin = os.Stdin
-			c.Stdout = os.Stdout
-			var buf bytes.Buffer
-			c.Stderr = io.MultiWriter(os.Stderr, &buf)
-			return c.Run()
+			return Execute(cmdStruct.MapsTo.Name, args)
 		},
 	}
-
-	// add Flags
-	if cmdStruct.Flags != nil && len(cmdStruct.Flags) > 0 {
-		for _, f := range cmdStruct.Flags {
-			fs := cmd.Flags()
-			switch f.Type {
-			case "string":
-				fs.StringP(f.Name, f.Alias, f.DefaultValue, f.Description)
-			case "bool":
-				v, _ := strconv.ParseBool(f.DefaultValue)
-				fs.BoolP(f.Name, f.Alias, v, f.Description)
-			case "int":
-				v, _ := strconv.Atoi(f.DefaultValue)
-				fs.IntP(f.Name, f.Alias, v, f.Description)
-			}
-		}
-	}
-
+	addFlags(cmdStruct, cmd)
 	return cmd
 }
 
-// Add the cobra command in Host CLI
+// Execute external process/command
+func Execute(executablePath string, cmdArgs []string) error {
+	cmd := exec.Command(executablePath, cmdArgs...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	return cmd.Run()
+}
+
+// TODO: Wrap flags with plugin's binary.
+// add Flags to the command
+func addFlags(cmdStruct *CommandConfig, cmd *cobra.Command) {
+	if cmdStruct.Flags != nil && len(cmdStruct.Flags) > 0 {
+		for _, flag := range cmdStruct.Flags {
+			switch flag.Type {
+			case "string":
+				cmd.Flags().StringP(flag.Name, flag.Alias, flag.DefaultValue, flag.Description)
+			case "bool":
+				v, _ := strconv.ParseBool(flag.DefaultValue)
+				cmd.Flags().BoolP(flag.Name, flag.Alias, v, flag.Description)
+			case "int":
+				v, _ := strconv.Atoi(flag.DefaultValue)
+				cmd.Flags().IntP(flag.Name, flag.Alias, v, flag.Description)
+			}
+		}
+	}
+}
+
+// Add the cobra commands in Host CLI
 func LoadCommands(cmd *cobra.Command) {
-	dateCommand := createCommand(&CommandConfig{
+	dateCommand := createCommandNew(&CommandConfig{
 		Name:             "whatsup",
 		MapsTo:           ArgsConfig{Name: "date", Subcommand: "-u", Args: []string{}},
 		Flags:            []FlagConfig{},
@@ -62,7 +69,7 @@ func LoadCommands(cmd *cobra.Command) {
 		Examples:         "$ host whatsup",
 	})
 
-	yarnCommand := createCommand(&CommandConfig{
+	yarnCommand := createCommandNew(&CommandConfig{
 		Name:   "install",
 		MapsTo: ArgsConfig{Name: "yarn", Subcommand: "add", Args: []string{"package-name"}},
 		Flags: []FlagConfig{
@@ -79,6 +86,24 @@ func LoadCommands(cmd *cobra.Command) {
 		Examples:         "$ host install",
 	})
 
+	addCommand := createCommandNew(&CommandConfig{
+		Name:             "plus",
+		MapsTo:           ArgsConfig{Name: "./plugins/calc", Subcommand: "add", Args: []string{"num1", "num2"}},
+		ShortDescription: "Adds Two Nums",
+		Examples:         "$ host plus 1 2",
+		Flags: []FlagConfig{
+			{
+				Name:         "fl",
+				Alias:        "f",
+				DefaultValue: "false",
+				Type:         "bool",
+				Description:  "Add floating numbers",
+				MapsTo:       "float",
+			},
+		},
+	})
+
 	cmd.AddCommand(dateCommand)
 	cmd.AddCommand(yarnCommand)
+	cmd.AddCommand(addCommand)
 }
