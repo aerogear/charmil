cff5ee08ba51af0233382ece163dc33edfa5096a
Ankit Hans <ankithans1947@gmail.com>
2021-06-17 18:20:39 +0530

    docs: provide i18n docs

--- pkg/localize/localize.go
+++ pkg/localize/localize.go
@@ -1,5 +1,7 @@
 package localize
 
+// Interface for localizing messages from locals
 type Localizer interface {
+	// localize by providing id of text in locals
 	LocalizeByID(messageId string) string
 }
