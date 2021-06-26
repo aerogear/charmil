265b1d12be004aebb2c4c336d16f521eb075d1f0
Ankit Hans <ankithans1947@gmail.com>
2021-06-26 09:49:57 +0530

    fix: validate during reflecting

--- validator/error.go
+++ validator/error.go
@@ -3,14 +3,15 @@ package validator
 import "errors"
 
 var (
-	ErrMin         = errors.New("less than min")
-	ErrMax         = errors.New("less than max")
-	ErrLen         = errors.New("invalid length")
-	ErrNeg         = errors.New("negative value")
-	ErrRegexp      = errors.New("regular expression mismatch")
-	ErrUnsupported = errors.New("unsupported type")
-	ErrZeroValue   = errors.New("zero value")
-	ErrAbsent      = errors.New("field must be present")
+	ErrMin           = errors.New("less than min")
+	ErrMax           = errors.New("less than max")
+	ErrLen           = errors.New("invalid length")
+	ErrNeg           = errors.New("negative value")
+	ErrRegexp        = errors.New("regular expression mismatch")
+	ErrUnsupported   = errors.New("unsupported type")
+	ErrZeroValue     = errors.New("zero value")
+	ErrAbsent        = errors.New("field must be present")
+	ErrFieldNotExist = errors.New("field doesn't exists")
 )
 
 type Error struct {
