58b5d2fe21516eacea91d5885ebf81b573346916
Ankit Hans <ankithans1947@gmail.com>
2021-06-26 10:41:39 +0530

    docs: dev docs for validator

--- validator/validator.go
+++ validator/validator.go
@@ -4,6 +4,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// Rule is the interface which is implemented
+// by every rule defined in validator package
 type Rule interface {
+	// Validate validates the cobra command
+	// and returns errors according to the provided rules
+	// verbose option is present which prints debug info
 	Validate(cmd *cobra.Command, verbose bool) []Error
 }
