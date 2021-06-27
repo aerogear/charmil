cdf969c0401e7acd5c1d73f50cee1370d3b1cea2
Ankit Hans <ankithans1947@gmail.com>
2021-06-27 18:36:47 +0530

    chore: refactor validator

--- validator/rules/length.go
+++ validator/rules/length.go
@@ -1,10 +1,22 @@
 package rules
 
 import (
+	"errors"
 	"fmt"
 	"reflect"
 
+	"github.com/aerogear/charmil/validator"
 	"github.com/spf13/cobra"
+)
+
+// define errors for the Length Rule
+var (
+	ErrLengthMin           = errors.New("less than min")
+	ErrLengthMax           = errors.New("less than max")
+	ErrLengthInvalid       = errors.New("invalid length")
+	ErrLengthNegative      = errors.New("negative value")
+	ErrLengthZeroValue     = errors.New("zero value")
+	ErrLengthFieldNotExist = errors.New("field doesn't exists")
 )
 
 var LengthRule = "LENGTH_RULE"
@@ -22,23 +34,23 @@ type Limit struct {
 }
 
 // Validate implements the Rule interface
-func (l *Length) ValidateLength(cmd *cobra.Command, verbose bool) []ValidationError {
-	var errors []ValidationError
-	info := StatusLog{totalTested: 0, totalErrors: 0, errors: errors}
+func (l *Length) ValidateLength(cmd *cobra.Command, verbose bool) []validator.ValidationError {
+	var errors []validator.ValidationError
+	info := validator.StatusLog{TotalTested: 0, TotalErrors: 0, Errors: errors}
 
-	return Traverse(
+	return validator.Traverse(
 		cmd,
 		verbose,
 		info,
 		l,
-		func(cmd *cobra.Command, verbose bool) []ValidationError {
+		func(cmd *cobra.Command, verbose bool) []validator.ValidationError {
 			return l.validateHelper(cmd, verbose)
 		},
 	)
 }
 
-func (l *Length) validateHelper(cmd *cobra.Command, verbose bool) []ValidationError {
-	var errors []ValidationError
+func (l *Length) validateHelper(cmd *cobra.Command, verbose bool) []validator.ValidationError {
+	var errors []validator.ValidationError
 
 	for fieldName, limits := range l.Limits {
 		// reflects the fieldName in cobra.Command struct
@@ -46,7 +58,7 @@ func (l *Length) validateHelper(cmd *cobra.Command, verbose bool) []ValidationEr
 
 		// if the defined fieldName doesn't exist in cobra.Command
 		if reflectValue.String() == "<invalid Value>" {
-			errors = append(errors, ValidationError{Name: fmt.Sprintf("%s Field doesn't exist in cobra.Command", fieldName), Err: ErrFieldNotExist, Rule: LengthRule, cmd: cmd})
+			errors = append(errors, validator.ValidationError{Name: fmt.Sprintf("%s Field doesn't exist in cobra.Command", fieldName), Err: ErrLengthFieldNotExist, Rule: LengthRule, Cmd: cmd})
 			continue
 		}
 
@@ -61,7 +73,7 @@ func (l *Length) validateHelper(cmd *cobra.Command, verbose bool) []ValidationEr
 
 // validateField compares the defined limit
 // with the length of the attribute/value
-func validateField(cmd *cobra.Command, limit Limit, value string, path string, verbose bool) ValidationError {
+func validateField(cmd *cobra.Command, limit Limit, value string, path string, verbose bool) validator.ValidationError {
 	length := len(value)
 
 	// check if valid limit is set
@@ -76,27 +88,27 @@ func validateField(cmd *cobra.Command, limit Limit, value string, path string, v
 	}
 
 	if length < limit.Min {
-		return ValidationError{Name: fmt.Sprintf("length should be at least %d", limit.Min), Err: ErrMin, Rule: LengthRule, cmd: cmd}
+		return validator.ValidationError{Name: fmt.Sprintf("length should be at least %d", limit.Min), Err: ErrLengthMin, Rule: LengthRule, Cmd: cmd}
 	}
 	if length > limit.Max {
-		return ValidationError{Name: fmt.Sprintf("length should be less than %d", limit.Max), Err: ErrMax, Rule: LengthRule, cmd: cmd}
+		return validator.ValidationError{Name: fmt.Sprintf("length should be less than %d", limit.Max), Err: ErrLengthMax, Rule: LengthRule, Cmd: cmd}
 	}
 
-	return ValidationError{}
+	return validator.ValidationError{}
 }
 
 // isLimitSet checks if the limit set
 // by the user is valid or not
-func isLimitSet(cmd *cobra.Command, limit Limit) (bool, ValidationError) {
+func isLimitSet(cmd *cobra.Command, limit Limit) (bool, validator.ValidationError) {
 	if limit.Max < 0 || limit.Min < 0 {
-		return true, ValidationError{Name: "max and min must be greater than 0", Err: ErrNeg, Rule: LengthRule, cmd: cmd}
+		return true, validator.ValidationError{Name: "max and min must be greater than 0", Err: ErrLengthNegative, Rule: LengthRule, Cmd: cmd}
 	}
 	if limit.Max == 0 && limit.Min == 0 {
-		return false, ValidationError{Name: "limit not set", Err: ErrZeroValue, Rule: LengthRule, cmd: cmd}
+		return false, validator.ValidationError{Name: "limit not set", Err: ErrLengthZeroValue, Rule: LengthRule, Cmd: cmd}
 	}
 	if limit.Max < limit.Min {
-		return true, ValidationError{Name: "max limit must be greater than min limit", Err: ErrMin, Rule: LengthRule, cmd: cmd}
+		return true, validator.ValidationError{Name: "max limit must be greater than min limit", Err: ErrLengthMin, Rule: LengthRule, Cmd: cmd}
 	}
 
-	return true, ValidationError{}
+	return true, validator.ValidationError{}
 }
