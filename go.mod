034a051f0cea6ed880ae51a6040b175ce6c161b2
Ankit Hans <ankithans1947@gmail.com>
2021-07-23 08:38:24 +0000

    feat: charmil init command (#170)
    * feat: add prompts in init
    
    * feat: clone starter git repo
    
    * feat: render template after cloning
    
    * feat: apply template to cloned starter

--- go.mod
+++ go.mod
@@ -5,16 +5,15 @@ go 1.16
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/fatih/color v1.12.0
+	github.com/go-git/go-git/v5 v5.4.2
 	github.com/imdario/mergo v0.3.12
-	github.com/kr/text v0.2.0 // indirect
+	github.com/manifoldco/promptui v0.8.0
 	github.com/mattn/go-isatty v0.0.13
 	github.com/nicksnyder/go-i18n/v2 v2.1.2
-	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/pelletier/go-toml v1.9.3
 	github.com/spf13/cobra v1.2.1
 	github.com/tidwall/sjson v1.1.7
 	golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40 // indirect
 	golang.org/x/text v0.3.6
-	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
--- pkg/cmd/init/init.go
+++ pkg/cmd/init/init.go
@@ -1,10 +1,34 @@
 package init
 
 import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+
+	"github.com/aerogear/charmil/core/color"
 	"github.com/aerogear/charmil/core/factory"
+	"github.com/go-git/go-git/v5"
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
 
+// promptContent is a struct to hold the prompt content
+type promptContent struct {
+	errorMsg string
+	label    string
+}
+
+// TemplateContext contains the context for rendering templates
+type TemplateContext struct {
+	Owner   string
+	Repo    string
+	CliName string
+}
+
+// InitCommand initializes the starter project
 func InitCommand(f *factory.Factory) *cobra.Command {
 
 	cmd := &cobra.Command{
@@ -13,8 +37,155 @@ func InitCommand(f *factory.Factory) *cobra.Command {
 		Long:          f.Localizer.LocalizeByID("init.cmd.long"),
 		Example:       f.Localizer.LocalizeByID("init.cmd.example"),
 		SilenceErrors: true,
-		Run:           func(cmd *cobra.Command, args []string) {},
+		Run: func(cmd *cobra.Command, args []string) {
+			owner := promptGetInput(promptContent{
+				label:    "GitHub Organization or Username",
+				errorMsg: "Please Provide a username",
+			})
+			repo := promptGetInput(promptContent{
+				label:    "GitHub Repo Name",
+				errorMsg: "Please Provide a repo name",
+			})
+			cli_name := promptGetInput(promptContent{
+				label:    "CLI Name",
+				errorMsg: "Please Provide a cli name",
+			})
+
+			templateContext := TemplateContext{
+				Owner:   owner,
+				Repo:    repo,
+				CliName: cli_name,
+			}
+
+			cloneStarter(f)
+			f.Logger.Infoln(color.Info("updating starter code with names"))
+			if err := renderTemplates(templateContext, f); err != nil {
+				f.Logger.Error(err)
+				os.Exit(1)
+			}
+			f.Logger.Infof(color.Success("Your %s CLI has been initialized in this directory.\n"), templateContext.CliName)
+		},
 	}
 
 	return cmd
+}
+
+// promptGetInput returns a string got by prompting the user
+func promptGetInput(pc promptContent) string {
+
+	// validate function for validating prompts
+	validate := func(input string) error {
+		if len(input) == 0 {
+			return errors.New(pc.errorMsg)
+		}
+		return nil
+	}
+
+	prompt := promptui.Prompt{
+		Label:    pc.label,
+		Validate: validate,
+	}
+
+	result, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		os.Exit(1)
+	}
+
+	return result
+}
+
+// clone a git repository to a local path using go-git
+func cloneStarter(f *factory.Factory) {
+	path, pathErr := os.Getwd()
+	if pathErr != nil {
+		f.Logger.Error(pathErr)
+		os.Exit(1)
+	}
+
+	_, cloneErr := git.PlainClone(path, false, &git.CloneOptions{
+		URL:      "https://github.com/ankithans/charmil-starter-template",
+		Progress: f.IOStreams.Out,
+	})
+	if cloneErr != nil {
+		f.Logger.Error(cloneErr)
+		os.Exit(1)
+	}
+}
+
+// render templates
+func renderTemplates(templateContext TemplateContext, f *factory.Factory) error {
+	path, pathErr := os.Getwd()
+	if pathErr != nil {
+		f.Logger.Error(pathErr)
+		os.Exit(1)
+	}
+
+	// rename cli name as given by user
+	oldPath := path + "/cmd/cli"
+	newPath := path + "/cmd/" + templateContext.CliName
+	if err := os.Rename(oldPath, newPath); err != nil {
+		return err
+	}
+
+	err := filepath.Walk(path,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			// ignore folders
+			ignoreSlice := []string{".git", ".github", ".chglog", ".goreleaser.yml", "CONTRIBUTING.md", "bin"}
+			pathSplit := strings.Split(path, string(os.PathSeparator))
+			intersected := intersection(pathSplit, ignoreSlice)
+			if len(intersected) > 0 {
+				return nil
+			}
+
+			fi, err := os.Stat(path)
+			if err != nil {
+				return fmt.Errorf("failed to read file info: %w", err)
+			}
+
+			if fi.IsDir() {
+				return nil
+			}
+
+			tmpl, tmplErr := template.ParseFiles(path)
+			if tmplErr != nil {
+				return fmt.Errorf("failed to parse template: %w", err)
+			}
+
+			f, PathErr := os.Create(path)
+			if PathErr != nil {
+				return fmt.Errorf("failed to create file: %w", err)
+			}
+
+			// apply templateContext to cloned repo
+			if err := tmpl.Execute(f, templateContext); err != nil {
+				return fmt.Errorf("failed to execute template: %w", err)
+			}
+
+			return nil
+		})
+	if err != nil {
+		return fmt.Errorf("failed to walk directory: %w", err)
+	}
+
+	return nil
+}
+
+// find intersection between two slices
+func intersection(s1, s2 []string) (inter []string) {
+	hash := make(map[string]bool)
+	for _, e := range s1 {
+		hash[e] = true
+	}
+	for _, e := range s2 {
+		// If elements present in the hashmap then append intersection list.
+		if hash[e] {
+			inter = append(inter, e)
+		}
+	}
+	return
 }
