203cdba9670a8acc1e5098a85b4f96f92718af7b
ankithans <ankithans1947@gmail.com>
2021-08-03 20:14:59 +0530

    fix: update starter url and imports

--- starter/cmd/cli/main.go
+++ starter/cmd/cli/main.go
@@ -6,9 +6,9 @@ import (
 	"log"
 	"os"
 
-	"github.com/aerogear/charmil/cli/internal/factory"
 	"github.com/aerogear/charmil/core/config"
 	"github.com/aerogear/charmil/core/localize"
+	"github.com/aerogear/charmil/starter/internal/factory"
 	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 	"golang.org/x/text/language"
